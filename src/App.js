b8d5079d2c59b3aae056281c0a6adc453a948c0b
alex <lolmagexe@gmail.com>
2022-05-18 17:17:41 +0300

    (feature) prepare App file mobile/tablet/desktop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import Card from 'components/01-molecules/Card';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Card description={"drinks"} name = {"Orange Juice"} price={"14.99"}></Card>
     </div>
-  );
+  )
 }
 
 export default App;
