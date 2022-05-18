4c47563223b87fe2843d1c8b44c145551fc02a36
alex <lolmagexe@gmail.com>
2022-05-18 17:19:48 +0300

    (feature) initial commit mobile/tablet/desktop

---
+++ src/components/Template/index.jsx
@@ -0,0 +1,11 @@
+import style from './index.module.css';
+
+import React from 'react'
+
+const Template = () => {
+  return (
+    <div className={style['template']}>template</div>
+  )
+}
+
+export default Template
