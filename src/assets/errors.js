115dd3d63720030c14c1aafb0ab63986e4aa31b1
alex <lolmagexe@gmail.com>
2022-05-19 13:04:30 +0300

    (feature) added assets folder mobile/tablet/desktop

---
+++ src/assets/errors.js
@@ -0,0 +1,5 @@
+
+export const ERROR_EMPTY =  "This field in required";
+export const ERROR_LETTERS_ONLY =  "Only letters allowed";
+export const ERROR_NUMBERS_ONLY = "Only numbers allowed";
+export const ERROR_SHOULD_HAVE_12_SYMBOLS = "Should contain 12 characters";
---
+++ src/assets/links.js
@@ -0,0 +1,1 @@
+export const LINK_CARDS = "https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e";
