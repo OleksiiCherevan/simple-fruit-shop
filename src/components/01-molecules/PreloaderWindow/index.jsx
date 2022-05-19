107295dc7d9a3e88a3139da2d07273525719d65f
alex <lolmagexe@gmail.com>
2022-05-19 13:10:04 +0300

    (feature) added preloader window  mobile/tablet/desktop

---
+++ src/components/01-molecules/PreloaderWindow/index.jsx
@@ -0,0 +1,15 @@
+import style from './index.module.css';
+import Preloader from 'components/00-atoms/Preloader';
+import React from 'react'
+
+const PreloaderWindow = (props) => {
+    const { children } = props;
+    
+  return (
+    <div className={style['preloader']}>
+      <Preloader></Preloader>
+    </div>
+  )
+}
+
+export default PreloaderWindow
