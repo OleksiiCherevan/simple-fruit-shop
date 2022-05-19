96d897e764a3bb9bab1c59288ab341fbb24b73a7
alex <lolmagexe@gmail.com>
2022-05-19 13:11:50 +0300

    (feature) added libraries mobile/tablet/desktop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import Card from 'components/01-molecules/Card';
 import './App.css';
+import TextField from 'components/00-atoms/TextField';
+import Button from 'components/00-atoms/Button';
+import Card from 'components/01-molecules/Card';
+import ConfirmForm from 'components/01-molecules/ConfirmForm';
+import PreloaderWindow from 'components/01-molecules/PreloaderWindow';
+import ModalConfirm from 'components/02-organisms/ModalConfirm';
+
+import { useEffect, useState } from 'react';
+
+import { LINK_CARDS } from 'assets/links';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchCards, setSelectedCard } from 'store/cardsSlice';
 
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { cards, status, error } = useSelector(state => state.cards);
+
+  const [isShowModal, setIsShowModal] = useState(false);
+
+  useEffect(() => {
+    dispatch(fetchCards());
+  }, [])
+
+  const onBuyClick = (e) => {
+    setIsShowModal(true);
+  }
+
+  const onBuyCheapest = e => {
+    const cheapest = [...cards].sort((cardA, cardB) =>  cardA.price - cardB.price).shift();
+    onSelectedCard(cheapest);
+    onShowModal();
+  }
+
+  const onCloseModal = (e) => {
+    setIsShowModal(false)
+  }
+  const onShowModal = (e) => {
+    setIsShowModal(true)
+  }
+
+  const onSelectedCard = card => {
+    dispatch(setSelectedCard({ card }))
+  }
+
+  if (error) {
+    return <h1>Happened something bad =</h1>
+  }
+
+  if (status === "pending") {
+    return <PreloaderWindow></PreloaderWindow>
+  }
+
   return (
     <div className='app'>
-      <Card description={"drinks"} name = {"Orange Juice"} price={"14.99"}></Card>
+      <div className="content">
+        {cards.map(card => 
+          <Card key={card.name} card = {card} onClick={onBuyClick}></Card>
+        )}
+      </div>
+
+      <Button onClick={onBuyCheapest}>Buy cheapest</Button>
+
+      {isShowModal ? <ModalConfirm onCloseModal={onCloseModal}></ModalConfirm> : ""}
     </div>
   )
 }
--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -2,7 +2,9 @@ import style from './index.module.css';
 
 import React from 'react'
 
-const Template = () => {
+const Template = (props) => {
+    const { children } = props;
+    
   return (
     <div className={style['template']}>template</div>
   )
