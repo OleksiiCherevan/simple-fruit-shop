ba388373631b55135abe50a3f2e5ae8913a22a31
alex <lolmagexe@gmail.com>
2022-05-19 13:09:01 +0300

    (feature) added  desktop view desktop

--- src/components/01-molecules/Card/index.jsx
+++ src/components/01-molecules/Card/index.jsx
@@ -1,27 +1,36 @@
 import style from './index.module.css';
 
 import React, { useState } from 'react'
-import TextCardDescription from 'components/00-atoms/TextCardDescription';
+import TextCardCategory from 'components/00-atoms/TextCardCategory';
 import TextCardPrice from 'components/00-atoms/TextCardPrice';
 import TextCardName from 'components/00-atoms/TextCardName';
 import ButtonSmall from 'components/00-atoms/ButtonSmall';
+import { useDispatch } from 'react-redux';
+import { setSelectedCard } from 'store/cardsSlice';
 
 const Card = (props) => {
-  const { children, description, name, price, link } = props;
+  const { children, link, card, onClick = () => { } } = props;
+  const {category, name, price} = card;
 
+  const dispatch = useDispatch();
   const [isHover, setIsHover] = useState(false);
 
+  const onBuyClick = e => {
+    onClick(e);
+    dispatch(setSelectedCard({card}));
+  }
+
   return (
-    <div className={style['card']} onMouseEnter = {e => setIsHover(true)} onMouseLeave = {e => setIsHover(false)}>
+    <div className={style['card']} onMouseEnter={e => setIsHover(true)} onMouseLeave={e => setIsHover(false)}>
 
       <div className={style['info']}>
-        <TextCardDescription>{description}</TextCardDescription>
+        <TextCardCategory>{category}</TextCardCategory>
         <TextCardName>{name}</TextCardName>
       </div>
 
       <div className={style['sell']}>
         <TextCardPrice>{price}</TextCardPrice>
-        <ButtonSmall isActive={isHover}>Buy</ButtonSmall>
+        <ButtonSmall isActive={isHover} onClick = {onBuyClick}>Buy</ButtonSmall>
       </div>
     </div>
   )
