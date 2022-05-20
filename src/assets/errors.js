89899adb1434611bb29fe5192c253c4bcd69fd9f
alex <lolmagexe@gmail.com>
2022-05-20 12:02:39 +0300

    (fix) updated syntax to eslint ES6 mobile/tablet/desktop

--- src/assets/errors.js
+++ src/assets/errors.js
@@ -1,5 +1,32 @@
+const COUNT_NUMBER_CHARACTERS = 12;
 
-export const ERROR_EMPTY =  "This field in required";
-export const ERROR_LETTERS_ONLY =  "Only letters allowed";
-export const ERROR_NUMBERS_ONLY = "Only numbers allowed";
-export const ERROR_SHOULD_HAVE_12_SYMBOLS = "Should contain 12 characters";
+const ERROR_EMPTY =  "This field in required";
+const ERROR_LETTERS_ONLY =  "Only letters allowed";
+const ERROR_NUMBERS_ONLY = "Only numbers allowed";
+const ERROR_SHOULD_HAVE_12_SYMBOLS = "Should contain 12 characters";
+
+
+const setMessageIfError = (text, errorMessage, onErrorRule, onSetMessage) => {
+    let isError = onErrorRule(text); // onCheckRule is error value
+  
+    if (isError) {
+      onSetMessage(errorMessage);
+    }
+
+    return isError;
+  };
+
+export const nameValidation = (text, onSetErrorMessage) => {
+    let isNotOnlyLetters = setMessageIfError(text, ERROR_LETTERS_ONLY, (text) => !new RegExp(/^[^0-9]*$/i).test(text), onSetErrorMessage);
+    let isEmpty = setMessageIfError(text, ERROR_EMPTY, (text) => text.length < 1, onSetErrorMessage);
+    
+    return isNotOnlyLetters || isEmpty;
+};
+
+export const numberValidation = (text, onSetErrorMessage) => {
+    let isNot12Chars = setMessageIfError(text, ERROR_SHOULD_HAVE_12_SYMBOLS, (text) => text.length !== COUNT_NUMBER_CHARACTERS, onSetErrorMessage);
+    let isNotOnlyNumbers = setMessageIfError(text, ERROR_NUMBERS_ONLY, (text) => !new RegExp(/^[0-9]*$/i).test(text), onSetErrorMessage);
+    let isEmpty = setMessageIfError(text, ERROR_EMPTY, (text) => text.length < 1, onSetErrorMessage);
+
+    return isNotOnlyNumbers || isEmpty || isNot12Chars;
+};
--- src/assets/svgRaw.js
+++ src/assets/svgRaw.js
@@ -2,15 +2,15 @@ export const FIELD_ERROR = <svg width="24" height="24" viewBox="0 0 24 24" fill=
   <circle cx="12" cy="12" r="12" fill="#E43F3F" />
   <path d="M17 7L7 17" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   <path d="M7 7L17 17" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-</svg>
+</svg>;
 
 export const ARROW_LEFT = <svg width="20"  viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M4.16663 10H15.8333"  strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 <path d="M10 4.16666L15.8333 9.99999L10 15.8333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-</svg>
+</svg>;
 
 export const CLOSE_CIRCLE = <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
   <circle cx="20" cy="20" r="20" fill="#F2F2F2" />
   <path d="M25 15L15 25" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   <path d="M15 15L25 25" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-</svg>
+</svg>;
